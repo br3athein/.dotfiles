4118035e93fd2892e03e21737767e36078f5964f
br3athein <br3athein@gmail.com>
2017-08-09 17:20:49 +0300

    awesome: Add awesome config (default)

---
+++ awesome/autoload.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function run {
+    if ! pgrep $1 ;
+    then
+        $@&
+    fi
+}
+
+# exec once on boot
+run kbdd
+run setxkbmap -layout us,ru -option 'grp:win_space_toggle'
+run pulseaudio --start
+
+run compton --config /home/br3athein/.compton.conf &
+
+# kick off apps in background
+run spotifywm
+run telegram-desktop
+run skypeforlinux
