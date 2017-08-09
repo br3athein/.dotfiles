ddf053970a00d9750fcb2dc6e856b247c3b3c8ec
br3athein <br3athein@gmail.com>
2017-08-09 17:38:19 +0300

    awesome: Change default terminal from `xterm' to `urxvt'

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -44,8 +44,8 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
 editor = os.getenv("EDITOR") or "nano"
+terminal = "urxvt"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
