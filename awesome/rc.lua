00cf76a9911402936a59bd00d22a4bf69e433672
br3athein <br3athein@gmail.com>
2017-08-10 11:44:10 +0300

    awesome: Add keybinds for 'lazy tag sniping'
    Sometimes you just don't want to remember 'that tag when I put that
    application on', you just want to get to it.
    With just one key, even though it's pressed like ten times. :3

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -239,6 +239,10 @@ globalkeys = gears.table.join(
               {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
               {description = "view next", group = "tag"}),
+    awful.key({ modkey,           }, ",",      awful.tag.viewprev,
+      {description = "view previous", group = "tag"}),
+    awful.key({ modkey,           }, ".",      awful.tag.viewnext,
+      {description = "view next", group = "tag"}),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
 
