7965f77016076e792ff99a0713996f49d6520b7b
br3athein <br3athein@gmail.com>
2018-09-30 00:27:43 +0300

    awesome: Reverse the rest of relative binds

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -268,20 +268,20 @@ globalkeys = gears.table.join(
               {description = "go back", group = "tag"}),
 
     awful.key({ modkey,           }, "j",
-        function () awful.client.focus.byidx(-1) end,
+        function () awful.client.focus.byidx( 1) end,
               {description = "focus next by index", group = "client"}
     ),
     awful.key({ modkey,           }, "k",
-        function () awful.client.focus.byidx( 1) end,
+        function () awful.client.focus.byidx(-1) end,
               {description = "focus previous by index", group = "client"}
     ),
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx( -1)    end,
+    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
               {description = "swap with next client by index", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx(  1)    end,
+    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
               {description = "swap with previous client by index", group = "client"}),
     awful.key({ modkey,           }, "h", function () awful.screen.focus_relative(-1) end,
               {description = "focus the next screen", group = "screen"}),
