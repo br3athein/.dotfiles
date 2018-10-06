03a6565447ecf183034c5536941887a5b232647a
br3athein <br3athein@gmail.com>
2018-10-06 14:13:22 +0300

    awesome: Allow killing clients w/ Mod4+Mouse2

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -479,6 +479,7 @@ end
 clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
+    awful.button({ modkey }, 2, function (c) c:kill() end),
     awful.button({ modkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
