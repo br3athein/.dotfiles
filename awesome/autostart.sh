f800eb2b38bf8e5afb8c738251db2f0abfc1db21
br3athein <br3athein@gmail.com>
2018-10-20 03:20:09 +0300

    autostart: Update it

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -8,9 +8,18 @@ function run {
 }
 
 # exec once on boot
-run kbdd && sleep 1
-urxvtd --quiet --fork
-run setxkbmap -layout us,ru -option 'grp:win_space_toggle' -option caps:swapescape
+perWindowLayoutD
+
+# Enter `sick bastard` mode by tossing key purposes across the kbd
+# using physical Caps Lock where you normally should have used Esc is vital for
+# Vim editing mode, IMO -- and, as Caps Lock is now on Esc and is a bit hard to
+# reach, hitting both shifts simultaneously would enable it instead.
+setxkbmap \
+    -option grp:win_space_toggle \
+    -option caps:swapescape \
+    -option shift:both_capslock \
+    -layout us,ru
+
 run pulseaudio --start
 
 run compton --config /home/br3athein/.compton.conf
@@ -18,4 +27,4 @@ run compton --config /home/br3athein/.compton.conf
 # kick off apps in background
 run gpmdp
 run telegram-desktop
-run skypeforlinux
+run unclutter -root  # doesn't play really well w/ awesome, thus xperimental
