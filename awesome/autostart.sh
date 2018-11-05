dacadb7780bddbc37306e6bcc518da96e2ee0631
br3athein <br3athein@gmail.com>
2018-11-05 03:42:54 +0200

    autostart/doc: Improve wording in sick bastard configuration doc
    "My tastes are very singular. You wouldn't understand." ©
    Lord, forgive me for citing it, but it fits just perfectly here.

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -10,10 +10,11 @@ function run {
 # exec once on boot
 perWindowLayoutD
 
-# Enter `sick bastard` mode by tossing key purposes across the kbd
-# using physical Caps Lock where you normally should have used Esc is vital for
-# Vim editing mode, IMO -- and, as Caps Lock is now on Esc and is a bit hard to
-# reach, hitting both shifts simultaneously would enable it instead.
+# Engage `sick bastard` mode by tossing key purposes across the whole keyboard
+# Using something handy in terms of reachability (physical Caps Lock, for instance),
+# where you should normally have used Esc is vital for Vim editing mode, IMO --
+# and, as Caps Lock is now bound to Esc and is somewhat hard to reach, hitting
+# both shifts simultaneously also will activate Caps Lock mode from now on.
 setxkbmap \
     -option grp:win_space_toggle \
     -option caps:swapescape \
