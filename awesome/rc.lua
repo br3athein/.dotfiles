76fbaa0f607165f2dd027b8d07977f8d00cb5224
br3athein <br3athein@gmail.com>
2018-11-05 02:59:08 +0200

    awesome: Add a blank

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -655,6 +655,7 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
 -- }}}
 
 -- {{{ Autostart
