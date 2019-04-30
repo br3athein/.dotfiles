6c610627b03494aec1e5ae909af07c1736b77c00
br3athein <br3athein@gmail.com>
2019-04-30 14:31:28 +0300

    awesome: Update taglist

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -205,7 +205,7 @@ awful.screen.connect_for_each_screen(function(s)
 
     -- Each screen has its own tag table.
     awful.tag({
-        "", "", "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "", "", "",
               }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
